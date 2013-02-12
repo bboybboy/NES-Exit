f4a25f94ad596d448f2820381390dbb096783253
Eric Conner <eric@ericconnerapps.com>
2013-02-12 12:59:07 -0500

    Update NES-Exit.py

--- NES-Exit.py
+++ NES-Exit.py
@@ -1,7 +1,6 @@
 """ NES-exit.py by Eric Conner on 2-6-2013
 GPIO Keyboard driver for Raspberry Pi for use with external reset button on NES
 """
-
 import uinput
 import time
 import RPi.GPIO as GPIO
@@ -10,12 +9,22 @@ GPIO.setmode(GPIO.BOARD)
 
 pin_esc = 3
 
+# esc, h, f2, f3, f6, f7, p
 GPIO.setup(pin_esc, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-while True:
-	if (GPIO.input(pin_esc) == 0):	# ESC button pressed
-		device.emit(uinput.KEY_ESC, 1)
-	else:
-		device.emit(uinput.KEY_ESC, 0)
+events = (uinput.KEY_ESC)
 
-	time.sleep(.05)  # Poll every 50ms (otherwise CPU load gets too high)
+device = uinput.Device(events)
+
+# Bools to keep track of movement
+key_esc = False
+
+while True:
+  if (not key_esc) and (not GPIO.input(pin_esc)):  # ESC button pressed
+    key_esc = True
+    device.emit(uinput.KEY_ESC, 1)
+  if key_esc and GPIO.input(pin_esc):              # ESC button released
+    key_esc = False
+    device.emit(uinput.KEY_ESC, 0) 
+
+  time.sleep(.1)  # Poll every 100ms (otherwise CPU load gets too high)
